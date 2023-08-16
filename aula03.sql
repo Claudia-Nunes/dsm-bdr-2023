fd5436b575a23f861eb608b3b5fa5d3a6f8f22f7
Claudia Nunes <claudia.dn@outlook.com>
2023-08-15 22:58:38 -0300

    Create aula03.sql
    criação bd_aula 3

---
+++ aula03.sql
@@ -0,0 +1,25 @@
+CREATE TABLE tbl_cliente (
+
+codigo_cliente integer PRIMARY KEY,
+nome text NOT NULL,
+cidade text,
+endereco text);
+
+CREATE TABLE tbl_titulo (
+
+codigo_titulo integer PRIMARY KEY,
+titulo text NOT NULL,
+descricao text,
+categoria text);
+
+CREATE TABLE tbl_emprestimo (
+
+numero_emprestimo integer PRIMARY KEY,
+codigo_client int,
+codigo_livro int);
+
+CREATE TABLE tbl_livros (
+
+cod_livro integer PRIMARY KEY,
+codigo_titulo int,
+status text);
